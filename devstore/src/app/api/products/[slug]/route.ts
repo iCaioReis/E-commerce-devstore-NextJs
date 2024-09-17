4f4f22a730ce0802903effb9fda4479043df5131
iCaioReis <caioreis3186@gmail.com>
2024-09-17 17:54:26 -0300

    Featch data in product page

---
+++ devstore/src/app/api/products/[slug]/route.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+import data from '../data.json'
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } },
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const slug = z.string().parse(params.slug)
+
+  const product = data.products.find((product) => product.slug === slug)
+
+  if (!product) {
+    return Response.json({ message: 'Product not found' }, { status: 400 })
+  }
+
+  // const featuredProducts = data.products.filter((product) => product.featured)
+
+  return Response.json(product)
+}
