76070e5b6173c1cec2a70410112ca5065bc38fca
iCaioReis <caioreis3186@gmail.com>
2024-09-25 11:08:37 -0300

    Search products in API

--- devstore/src/app/(store)/(home)/page.tsx
+++ devstore/src/app/(store)/(home)/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
     next: {
-      revalidate: 6,
+      revalidate: 60 * 60,
     },
   })
 
